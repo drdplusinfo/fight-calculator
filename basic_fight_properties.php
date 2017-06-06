e2ab0b126428133db7dffed4b8b06b2b8ac078da
Jaroslav Týc <mail@jaroslavtyc.com>
2017-06-06 14:53:55 +0200

    Armor and helm protection is shown

--- basic_fight_properties.php
+++ basic_fight_properties.php
@@ -1,3 +1,10 @@
+<?php
+namespace DrdPlus\Fight;
+
+use DrdPlus\Tables\Tables;
+
+/** @var Controller $controller */
+?>
 <h2 id="Obecně"><a class="inner" href="#Obecně">Obecně</a></h2>
 <table class="block result shortened">
     <?php $fightProperties = $controller->getGenericFightProperties() ?>
@@ -21,6 +28,16 @@
         <td>OČ <img class="line-sized" src="images/emojione/defense-number-1f6e1.png"></td>
         <td><?= $fightProperties->getDefenseNumber() ?></td>
         <td><span class="hint">(ovlivněno pouze akcí, oslněním a Převahou)</span></td>
+    </tr>
+    <tr>
+        <td>Zbroj <img class="line-sized" src="images/armor-icon.png"></td>
+        <td><?= Tables::getIt()->getBodyArmorsTable()->getProtectionOf($controller->getSelectedBodyArmor()) ?></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>Helma <img class="line-sized" src="images/helm-icon.png"></td>
+        <td><?= Tables::getIt()->getHelmsTable()->getProtectionOf($controller->getSelectedHelm()) ?></td>
+        <td></td>
     </tr>
     </tbody>
 </table>
