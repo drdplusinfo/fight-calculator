5043dee4ff2e5616e1d7314605aec1d28f784387
Jaroslav Týc <mail@jaroslavtyc.com>
2017-06-08 18:59:22 +0200

    Also currently changed skill rank value is remember, just for current session

--- js/main.js
+++ js/main.js
@@ -1,27 +1,37 @@
 window.addEventListener('load', function () {
-    let withSkillRanksNodes = document.getElementsByClassName('with-skill-ranks');
-    for (let nodeIndex = 0, nodesLength = withSkillRanksNodes.length; nodeIndex < nodesLength; nodeIndex++) {
-        let withSkillRanksNode = withSkillRanksNodes[nodeIndex];
-        let select = withSkillRanksNode.getElementsByTagName('select')[0];
-        let skillRanksRaw = withSkillRanksNode.getElementsByClassName('skill-ranks')[0];
-        let previousSkillRanks = JSON.parse(skillRanksRaw.dataset.previousSkillRanks);
-        select.addEventListener('change', function (event) {
-            let selectedValue = event.target.value;
-            let previousSkillRankValue = null;
-            if (previousSkillRanks.hasOwnProperty(selectedValue)) {
-                previousSkillRankValue = previousSkillRanks[selectedValue].toString();
-            }
-            if (previousSkillRankValue === null) {
-                previousSkillRankValue = '0';
-            }
+        let withSkillRanksNodes = document.getElementsByClassName('with-skill-ranks');
+        for (let nodeIndex = 0, nodesLength = withSkillRanksNodes.length; nodeIndex < nodesLength; nodeIndex++) {
+            let withSkillRanksNode = withSkillRanksNodes[nodeIndex];
+            let select = withSkillRanksNode.getElementsByTagName('select')[0];
+            let previousSkillRanksNode = withSkillRanksNode.getElementsByClassName('skill-ranks')[0];
+            let previousSkillRanks = JSON.parse(previousSkillRanksNode.dataset.previousSkillRanks);
             let skillRankInputs = withSkillRanksNode.getElementsByTagName('input');
+            select.addEventListener('change', function (event) {
+                let selectedValue = event.target.value;
+                let previousSkillRankValue = null;
+                if (previousSkillRanks.hasOwnProperty(selectedValue)) {
+                    previousSkillRankValue = previousSkillRanks[selectedValue].toString();
+                }
+                if (previousSkillRankValue === null) {
+                    previousSkillRankValue = '0';
+                }
+                for (let rankIndex = 0, ranksLength = skillRankInputs.length; rankIndex < ranksLength; rankIndex++) {
+                    let skillRankInput = skillRankInputs[rankIndex];
+                    if (skillRankInput.value === previousSkillRankValue) {
+                        skillRankInput.checked = true;
+                        return;
+                    }
+                }
+            });
             for (let rankIndex = 0, ranksLength = skillRankInputs.length; rankIndex < ranksLength; rankIndex++) {
                 let skillRankInput = skillRankInputs[rankIndex];
-                if (skillRankInput.value === previousSkillRankValue) {
-                    skillRankInput.checked = true;
-                    return;
-                }
+                skillRankInput.addEventListener('change', function (event) {
+                    if (!event.target.checked) {
+                        return;
+                    }
+                    previousSkillRanks[select.value] = event.target.value;
+                });
             }
-        });
+        }
     }
-});
+);
