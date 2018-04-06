39128b9fa36a8fd3151ee534ae0eaa1dd4dabd2b
Jaroslav Týc <mail@jaroslavtyc.com>
2018-04-06 15:49:18 +0200

    Every basic parameter can be changed by step of 1

--- parts/profession_and_body_properties.php
+++ parts/profession_and_body_properties.php
@@ -19,39 +19,39 @@ use DrdPlus\Codes\ProfessionCode;
 <div class="panel body-properties">
     <div class="panel">
         <div class="block"><label for="strength">Síla</label></div>
-        <div class="block"><input id="strength" type="number" name="<?= Controller::STRENGTH ?>" min="-40" max="40"
+        <div class="block"><input id="strength" type="number" name="<?= Controller::STRENGTH ?>" min="-40" max="40" step="1"
                                   value="<?= $controller->getCurrentProperties()->getCurrentStrength()->getValue() ?>">
         </div>
     </div>
     <div class="panel">
         <div class="block"><label for="agility">Obratnost</label></div>
-        <div class="block"><input id="agility" type="number" name="<?= Controller::AGILITY ?>" min="-40" max="40"
+        <div class="block"><input id="agility" type="number" name="<?= Controller::AGILITY ?>" min="-40" max="40" step="1"
                                   value="<?= $controller->getCurrentProperties()->getCurrentAgility()->getValue() ?>">
         </div>
     </div>
     <div class="panel">
         <div class="block"><label for="knack">Zručnost</label></div>
-        <div class="block"><input id="knack" type="number" name="<?= Controller::KNACK ?>" min="-40" max="40"
+        <div class="block"><input id="knack" type="number" name="<?= Controller::KNACK ?>" min="-40" max="40" step="1"
                                   value="<?= $controller->getCurrentProperties()->getCurrentKnack()->getValue() ?>">
         </div>
     </div>
     <div class="panel">
         <div class="block"><label for="will">Vůle</label></div>
-        <div class="block"><input id="will" type="number" name="<?= Controller::WILL ?>" min="-40" max="40"
+        <div class="block"><input id="will" type="number" name="<?= Controller::WILL ?>" min="-40" max="40" step="1"
                                   value="<?= $controller->getCurrentProperties()->getCurrentWill()->getValue() ?>">
         </div>
     </div>
     <div class="panel">
         <div class="block"><label for="intelligence">Inteligence</label></div>
         <div class="block">
-            <input id="intelligence" type="number" name="<?= Controller::INTELLIGENCE ?>" min="-40" max="40"
+            <input id="intelligence" type="number" name="<?= Controller::INTELLIGENCE ?>" min="-40" max="40" step="1"
                    value="<?= $controller->getCurrentProperties()->getCurrentIntelligence()->getValue() ?>">
         </div>
     </div>
     <div class="panel">
         <div class="block"><label for="charisma">Charisma</label></div>
         <div class="block">
-            <input id="charisma" type="number" name="<?= Controller::CHARISMA ?>" min="-40" max="40"
+            <input id="charisma" type="number" name="<?= Controller::CHARISMA ?>" min="-40" max="40" step="1"
                    value="<?= $controller->getCurrentProperties()->getCurrentCharisma()->getValue() ?>"></div>
     </div>
     <div class="panel">
@@ -64,7 +64,7 @@ use DrdPlus\Codes\ProfessionCode;
     </div>
     <div class="panel">
         <div class="block"><label for="size">Velikost</label></div>
-        <div class="block"><input id="size" type="number" name="<?= Controller::SIZE ?>" min="-10" max="10"
+        <div class="block"><input id="size" type="number" name="<?= Controller::SIZE ?>" min="-10" max="10" step="1"
                                   value="<?= $controller->getCurrentProperties()->getCurrentSize()->getValue() ?>">
         </div>
     </div>
